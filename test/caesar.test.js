e03777b1f519c3d6bf7b02f467bbee8a87baedd0
aimon <aimonflaherty@gmail.com>
2023-01-31 20:55:48 -0500

    decoding caseser test cases done

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -37,8 +37,8 @@ describe("caesar tests", () => {
             expect(actual).to.equal(expected);
         });
         it("should return 'f g.h' for letters 'a b.c' and shift ammount 5 ", () => {
-            const expected = "fgh";
-            const actual = caesar("abc", 5);
+            const expected = "f g.h";
+            const actual = caesar("a b.c", 5);
             expect(actual).to.equal(expected);
         });
     });
@@ -57,6 +57,62 @@ describe("caesar tests", () => {
         });
         it("should return false for shift ammount < 25", () => { 
             expect(caesar("abc", 713)).to.be.false;
+        });
+    });
+    describe("Decoding edge cases", () => {
+        it("should return 'a' for letter 'z' and shift ammount -1", () => {
+            const expected = "a";
+            const actual = caesar("z", -1, false);
+            expect(actual).to.equal(expected);
+        });
+        it("should return 'yz' for letter 'ab' and shift ammount 2", () => {
+            const expected = "yz";
+            const actual = caesar("ab", 2, false);
+            expect(actual).to.equal(expected);
+        });
+    });
+    describe("Decoding Normal cases", () => {
+        it("should return 'abc' for letters 'fgh' and shift ammount 5 ", () => {
+            const expected = "abc";
+            const actual = caesar("fgh", 5, false);
+            expect(actual).to.equal(expected);
+        });
+        it("should return 'fgh' for letters 'abc' and shift ammount -5 ", () => {
+            const expected = "fgh";
+            const actual = caesar("abc", -5, false);
+            expect(actual).to.equal(expected);
+        });
+        it("should return 'jrswu' for letters 'cklpn' and shift ammount -7", () => {
+            const expected = "jrswu";
+            const actual = caesar("cklpn", -7, false);
+            expect(actual).to.equal(expected);
+        });
+        it("should return 'abc' for letters 'FGH' and shift ammount 5 ", () => {
+            const expected = "abc";
+            const actual = caesar("FGH", 5, false);
+            expect(actual).to.equal(expected);
+        });
+        it("should return 'a b.c' for letters 'f g.h' and shift ammount 5 ", () => {
+            const expected = "a b.c";
+            const actual = caesar("f g.h", 5, false);
+            expect(actual).to.equal(expected);
+        });
+    });
+    describe("Decoding error cases", () => {
+        it("should return false for shift ammount 0", () => { 
+            expect(caesar("abc", 0, false)).to.be.false;
+        });
+        it("should return false for shift ammount < -25", () => { 
+            expect(caesar("abc", -26, false)).to.be.false;
+        });
+        it("should return false for shift ammount < -25", () => { 
+            expect(caesar("abc", -9016, false)).to.be.false;
+        });
+        it("should return false for shift ammount > 25", () => { 
+            expect(caesar("abc", 26, false)).to.be.false;
+        });
+        it("should return false for shift ammount < 25", () => { 
+            expect(caesar("abc", 713, false)).to.be.false;
         });
     });
 });
